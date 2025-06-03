29332eb0392334e3f6d00b72ab5d599da8316f4e
Aymen21916 <aymenhamidi111@gmail.com>
2025-06-03 23:08:53 +0100

    adding the first project

---
+++ src/app/projects.js
@@ -0,0 +1,52 @@
+const projects_data = {
+    1 : {
+        "image": "recipe-app.png",
+        "name": "Recipe App",
+        "source-code": "https://github.com/Aymen21916/recipe-app/tree/main",
+        "url": "https://recipe-app-lake-nu.vercel.app/",
+        "description": "Recipe app that allows users to search and save their favourite recipes. It features a clean and modern design.",
+        "tech": ['React', 'Next.js', 'Tailwind CSS', 'Edamam API']
+    },
+    2 : {
+        "image": "",
+        "name": "Project 2",
+        "source-code": "",
+        "url": "",
+        "description": "Upcoming project details will be added here.",
+        "tech": ['React', 'Next.js', 'Tailwind CSS']
+    },
+    3 : {
+        "image": "",
+        "name": "Project 3",
+        "source-code": "",
+        "url": "",
+        "description": "Upcoming project details will be added here.",
+        "tech": ['React', 'Next.js', 'Tailwind CSS']
+    },
+    4 : {
+        "image": "",
+        "name": "Project 4",
+        "source-code": "",
+        "url": "",
+        "description": "Upcoming project details will be added here.",
+        "tech": ['React', 'Next.js', 'Tailwind CSS']
+    },
+    5 : {
+        "image": "",
+        "name": "Project 5",
+        "source-code": "",
+        "url": "",
+        "description": "Upcoming project details will be added here.",
+        "tech": ['React', 'Next.js', 'Tailwind CSS']
+    },
+    6 : {
+        "image": "",
+        "name": "Project 6",
+        "source-code": "",
+        "url": "",
+        "description": "Upcoming project details will be added here.",
+        "tech": ['React', 'Next.js', 'Tailwind CSS']
+    },
+}
+
+export default projects_data;
